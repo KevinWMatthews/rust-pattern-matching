33fbc5b113fc2bafaee3425c93cfef65863eaf45
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-09-01 13:34:12 -0700

    Add example of matching non-copy types

---
+++ src/non_copy_types.rs
@@ -0,0 +1,43 @@
+fn main() {
+    own_matched();
+    old_borrow_matched();
+}
+
+
+fn own_matched() {
+    let maybe_number = Some(Box::new(42));
+    // let maybe_number: Option<Box<i32>> = None;
+
+    match maybe_number {
+        Some(owns_box) => {
+            let x = *owns_box;
+            println!("Found something: {}", x);
+        },
+        None => println!("Found nothing"),
+    }
+
+    // Compiler error - use after move
+    /*
+    match maybe_number {
+        Some(_) => {},
+        None => {},
+    }
+    */
+}
+
+fn old_borrow_matched() {
+    // Old-style Rust? Deprecated by match ergonomics?
+    // Might be better to use:
+    // match &maybe_number
+
+    let maybe_number = Some(Box::new(42));
+    // let maybe_number: Option<Box<i32>> = None;
+
+    match maybe_number {
+        Some(ref borrows_box) => {      // Explicitly bind Box as a reference
+            let x = **borrows_box;
+            println!("Found something: {}", x);
+        },
+        None => println!("Found nothing"),
+    }
+}
